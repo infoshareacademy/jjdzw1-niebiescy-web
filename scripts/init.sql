ba9d55478f35070d2c2946dba68dcedfdbbc88f9
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-06-25 20:06:59 +0200

    Script init.sql updated.

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,12 +1,10 @@
-DROP DATABASE niebiescy;
-
-CREATE DATABASE niebiescy;
+CREATE SCHEMA IF NOT EXISTS niebiescy;
 
 USE niebiescy;
 
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
-    id_iser int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_user int NOT NULL AUTO_INCREMENT,
     login varchar(20) NOT NULL,
     password varchar(255) NOT NULL,
     name varchar(100) NOT NULL,
@@ -14,6 +12,7 @@ CREATE TABLE users
     email varchar(360) NOT NULL,
     phone varchar(12),
     type VARCHAR(10) NOT NULL,
-    isactive BOOLEAN DEFAULT 0 NOT NULL
+    isactive BOOLEAN DEFAULT 0 NOT NULL,
+    CONSTRAINT PK_User PRIMARY KEY (id_user)
 );
 
