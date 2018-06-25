8b63a29afb0670dd719c7ded3bb6c352cd64f291
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-06-25 23:01:23 +0200

    Script updated.

--- scripts/maven-structure.sh
+++ scripts/maven-structure.sh
@@ -7,6 +7,7 @@ mkdir -p src/main/resources     #	Application/Library resources
 mkdir -p src/main/resources/META-INF
 mkdir -p src/main/filters 	    #   Resource filter files
 mkdir -p src/main/webapp 	    #   Web application sources
+mkdir -p src/main/webapp/WEB-INF
 mkdir -p src/test/java 	        #   Test sources
 mkdir -p src/test/resources 	#   Test resources
 mkdir -p src/test/filters 	    #   Test resource filter files
