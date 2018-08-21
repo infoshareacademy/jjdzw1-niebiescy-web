6298e88c32c000cd002dcbd8691d0df904ea97bd
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-08-21 10:38:49 +0200

    HomePageServlet - logika strony startowej.

---
+++ src/main/java/pl/findevent/servlets/ErrorPageServlet.java
@@ -0,0 +1,48 @@
+package pl.findevent.servlets;
+
+
+import pl.findevent.dao.UsersDao;
+
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@WebServlet("/Error")
+class ErrorPageServlet extends HttpServlet {
+
+    Logger logger = Logger.getLogger(getClass().getName());
+
+    @Inject
+    UsersDao usersDao;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        req.setAttribute("errorTitle", "Błąd - tytuł");
+        req.setAttribute("errorDecscription", "Błąd - opis");
+
+
+        req.setCharacterEncoding("UTF-8");
+        RequestDispatcher rd = req.getRequestDispatcher("/error.jsp");
+        rd.forward(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        RequestDispatcher rd = req.getRequestDispatcher("/error.jsp");
+        rd.forward(req, resp);
+
+
+    }
+}
