7d8617bd1f69ff9a95f37e64501ed5c88c2d2ec4
Marcin Jackowiak <marcin.jackowiak94@gmail.com>
2018-09-22 12:41:51 +0200

    Created image servlet

---
+++ src/main/java/pl/findevent/servlets/eventsservlets/EventImageServlet.java
@@ -0,0 +1,25 @@
+package pl.findevent.servlets.eventsservlets;
+
+import pl.findevent.cdi.ImageUpload;
+
+import javax.servlet.annotation.WebServlet;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+
+@WebServlet("/images/*")
+public class EventImageServlet extends HttpServlet {
+
+    @Inject
+    ImageUpload imageUpload;
+
+
+
+}
